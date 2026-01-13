bc150fcc307d89bc2d3be4038273f1dfa08b8158
miller <mil@cullenmiller.com>
2026-01-13 21:40:01 +0000

    minimal progress

--- accumulator_calc/test.py
+++ accumulator_calc/test.py
@@ -50,52 +50,81 @@ class Calculate_Pack_Parameters:
 
         # Calculate pack parameters from inputs
         self.configuration = f'{num_series/num_modules}s{num_parallel}p\t{num_modules}s'
-        self.Q_pack = num_parallel * cell_dict[cell_name]["Nominal Capacity (Ah)"]
-        self.nom_pack_v = num_series * cell_dict[cell_name]["Nominal Voltage (V)"]
-        self.max_pack_v = num_series * cell_dict[cell_name]["Max Voltage (V)"]
-        self.nom_mod_v = self.nom_pack_v / num_modules
-        self.max_mod_v = self.max_pack_v / num_modules
-        self.nom_pack_e =
-        self.max_pack_e
-        self.nom_mod_e
-        self.max_mod_e
-        self.max_pack_i
-        self.num_cells
-        self.mod_num_cells
-        self.cell_mass
-        self.mod_cell_mass
-        self.cell_volume
-        self.mod_volume
+        self.Q_pack = num_parallel * cell_dict[cell_name]["Nominal Capacity (Ah)"]  # V
+        self.nom_pack_v = num_series * cell_dict[cell_name]["Nominal Voltage (V)"]  # V
+        self.max_pack_v = num_series * cell_dict[cell_name]["Max Voltage (V)"]  # V
+        self.nom_mod_v = self.nom_pack_v / num_modules  # V
+        self.max_mod_v = self.max_pack_v / num_modules  # V
+        self.nom_pack_e = self.nom_pack_v * self.Q_pack /1000   # kWh
+        self.max_pack_e = self.max_pack_v * self.Q_pack /1000   # kWh
+        self.nom_mod_e = self.nom_mod_v * self.Q_pack /1000     # kWh
+        self.max_mod_e = self.max_mod_v * self.Q_pack /1000     # kWh
+        self.cont_pack_i = num_parallel * cell_dict[cell_name]["Continuous Discharge Current (A)"]  # A
+        self.num_cells = num_parallel * num_series
+        self.mod_num_cells = self.num_cells / num_modules
+        self.cell_mass = self.num_cells * cell_dict[cell_name]["Mass (g)"] / 1000   # kg
+        self.mod_cell_mass = self.cell_mass / num_modules   # kg
+        self.cell_volume = cell_dict[cell_name]["Volume (L)"] * self.num_cells  # L
+        self.mod_volume = self.cell_volume / num_modules    # L
+
+        self.pack_DCIR = ((cell_dict[cell_name]["Typical DCIR (mohm)"]*num_series)/num_parallel)*1000 # Ohms
+        p_loss = np.square(self.cont_pack_i) * self.pack_DCIR # This is an approximated power loss when operating at nominal values
+        self.nom_power = self.nom_pack_v * self.cont_pack_i
+        self.pack_efficiency = (self.nom_power - p_loss) / self.nom_power
 
 
-        self.data_to_write = [self.cell_name, self.configuration, self.Q_pack, self.nom_pack_v, self.max_pack_v, self.nom_mod_v, self.max_mod_v, self.nom_pack_e, self.max_pack_e, self.nom_mod_e, self.max_mod_e, self.max_pack_i, self.num_cells, self.mod_num_cells, self.cell_mass, self.mod_cell_mass ,self.cell_volume, self.mod_volume]
+        self.data_to_write = [
+            self.cell_name,
+            self.configuration,
+            self.Q_pack,
+            self.nom_pack_v,
+            self.max_pack_v,
+            self.nom_mod_v,
+            self.max_mod_v,
+            self.nom_pack_e,
+            self.max_pack_e,
+            self.nom_mod_e,
+            self.max_mod_e,
+            self.nom_power,
+            self.cont_pack_i,
+            self.num_cells,
+            self.mod_num_cells,
+            self.cell_mass,
+            self.mod_cell_mass ,
+            self.cell_volume,
+            self.mod_volume,
+            self.pack_DCIR,
+            self.pack_efficiency]
 
     def return_pack_parameters(self):
         '''
         indexes for pack parameters - used when checking externally
-        0 - cell name
-        1 - configuration
-        2 - pack capacity
-        3 - nominal pack voltage
-        4 - max pack voltage
-        5 - nom mod voltage
-        6 - max mod voltage
-        7 - nom pack e
-        8 - max pack e
-        9 - nom mod e
-        10 - max mod e
-        11 - max pack current
-        12 - total number of cells
-        13 - number of cells in each module
-        14 - mass of all cells
-        15 - mass of cells per module
-        16 - volume of all cells
-        17 - volume of cells per module
+        0   cell_name,
+        1   pack configuration (series / parallel and number of modules),
+        2   pack capacity (Ah),
+        3   nominal pack voltage (V),
+        4   maximum pack voltage (V),
+        5   nominal module voltage (V),
+        6   maximum module voltage (V),
+        7   nominal pack energy (kWh),
+        8   maximum pack energy (kWh),
+        9   nominal module energy (kWh),
+        10  maximum module energy (kWh),
+        11  nominal power output (W) - from nominal voltage and continuous rated current,
+        12  continuous rated current (A),
+        13  number of cells
+        14  number of cells per module,
+        15  cell mass (kg),
+        16  cell mass per module (kg),
+        17  cell volume (L),
+        18  cell module volume (L),
+        19  pack approx DCIR (ohm),
+        20  pack energy efficiency approximation
         '''
         return self.data_to_write # This can be used to check individual parameters externally so invalid layouts can be discarded
 
 
-    def save_to_file(self):
+    def write_to_dataframe(self):
         # Add row to the output file
 
 
